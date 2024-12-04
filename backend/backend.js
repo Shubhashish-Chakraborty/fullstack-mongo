9ea3f56c3fbd54a349713448b3a7fe8589e1bfb3
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-12-04 13:22:21 +0530

    login routing done || Connected to frontend

--- backend/backend.js
+++ backend/backend.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 
 const { UserModel } = require('./db')
+const {auth , JWT_SECRET} = require("./auth");
 
 const PORT = 3000;
 const app = express();
@@ -16,6 +17,7 @@ app.get('/' , (req , res) => {
     res.sendFile(path.join(__dirname, "../frontend/frontend.html"));
 })
 
+// Signup
 app.post('/signup' , async (req , res) => {
     const username = req.body.username;
     const email = req.body.email;
@@ -32,6 +34,33 @@ app.post('/signup' , async (req , res) => {
     })
 })
 
+
+// SignIN/LogIN
+app.post("/signin" , async (req , res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({
+        email: email,
+        password: password
+    })
+
+    if (!user) { // If User Not Found!!
+        res.status(403).json({
+            message: "User Not Found, Incorrect Credentials!!"
+        });
+    }
+    else { // IF user found -> allot a JWT and login
+        const token = jwt.sign({
+            id: user._id.toString()
+        } , JWT_SECRET);
+        
+        res.json({
+            msg: `${user.username} successfully LoggedIN!!`,
+            token: token
+        })
+    }
+})
 
 
 
